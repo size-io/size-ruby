a8234c414ec5e6109b135f4c814217d0dd24ae41
Gene Stevens <gene@spielengine.com>
2012-09-03 15:29:34 +0000

    Set Apache License 2.0

--- size/abstract_connector.rb
+++ size/abstract_connector.rb
@@ -1,3 +1,21 @@
+=begin
+
+   This file is provided to you under the Apache License,
+   Version 2.0 (the "License"); you may not use this file
+   except in compliance with the License.  You may obtain
+   a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+
+=end
+
 require 'multi_json'
 
 module Size
@@ -61,7 +79,7 @@ module Size
 			when :event
 				vals = {:k=>key, :v=>val}
 				vals[:t] = time if time
-				MultiJson.dump(vals) 
+				MultiJson.dump(vals)
 			else
 				raise SizeClient::Error, "Unrecognized message type: #{type.inspect}"
 			end
